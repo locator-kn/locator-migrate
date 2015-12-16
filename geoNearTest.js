d581c162f29d5e6f6264c874641e9e342386064c
Timo Weiß <info@timo-weiss.com>
2015-12-16 19:17:41 +0100

    codestyle, update limit

--- geoNearTest.js
+++ geoNearTest.js
@@ -5,14 +5,14 @@ MongoClient.connect('mongodb://localhost:27017/locator', (err, db) => {
     let collection = db.collection("locations");
 
     collection.ensureIndex({'geotag.coordinates':"2dsphere"}, (err, result) => {
-        collection.geoNear(9.169668, 47.668771, {limit: 15, maxDistance: 1000, spherical: true}, (err, docs) => {
+        collection.geoNear(9.169668, 47.668771, {limit: 1, maxDistance: 1000, spherical: true}, (err, docs) => {
             console.log(err || docs.results);
             docs.results.forEach(elem => {
                 console.log('distance:', elem.dis);
                 console.log('location:', elem.obj.title);
                 console.log('location:', elem.obj.geotag.coordinates);
 
-            })
+            });
             db.close();
         });
 
