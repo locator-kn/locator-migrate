d7d16ccc8f940c856333575d1795af3e84212a96
Timo Weiß <info@timo-weiss.com>
2015-12-21 14:42:06 +0100

    rewrite locations with new userid

--- transformer/locations.js
+++ transformer/locations.js
@@ -18,30 +18,43 @@ MongoClient.connect(url, function (err, db) {
 
     let collection = db.collection('locations');
 
-    fse.readJson('./olddata/locations.json', function (err, packageObj) {
-        let transformedUsers = [];
-        packageObj.forEach(elem => {
-            delete elem._rev;
-            delete elem.type;
-            delete elem._attachments;
-            elem.categories = [];
-            elem.favorites = elem.schoenhiers;
-            delete elem.schoenhiers;
+    fse.readJson('./olddata/userIdMapping.json', (err, userIdMappings) => {
+        if (err) {
+            throw err;
+        }
 
-            transformedUsers.push(elem);
-        });
+        fse.readJson('./olddata/locations.json', function (err, packageObj) {
+            let transformedUsers = [];
+            packageObj.forEach(elem => {
+                delete elem._rev;
+                delete elem.type;
+                delete elem._attachments;
+                delete elem._id;
+                userIdMappings.forEach(idObject => {
+                    if(elem.userid === idObject.oldId) {
+                        delete elem.userid;
+                        elem.user_id = idObject.newId;
+                    }
+                });
+                elem.categories = [];
+                elem.favorites = elem.schoenhiers || 0;
+                delete elem.schoenhiers;
 
-        insertImageAndDecorateObject(packageObj, 0, packageObj.length, newLocations => {
-            newLocations.forEach(location => {
-                delete location.images.picture;
-                location.geotag = { type: "Point", coordinates: [ location.geotag.long, location.geotag.lat ] }
+                transformedUsers.push(elem);
             });
-            collection.insertMany(newLocations).then(succ => {
-                console.log(succ);
-                db.close();
-            }).catch(err => {
-                db.close();
-                console.error(err);
+
+            insertImageAndDecorateObject(packageObj, 0, 5, newLocations => {
+                newLocations.forEach(location => {
+                    delete location.images.picture;
+                    location.geotag = {type: "Point", coordinates: [location.geotag.long, location.geotag.lat]}
+                });
+                collection.insertMany(newLocations).then(succ => {
+                    console.log(succ);
+                    db.close();
+                }).catch(err => {
+                    db.close();
+                    console.error(err);
+                });
             });
         });
     });
@@ -49,7 +62,7 @@ MongoClient.connect(url, function (err, db) {
 
 
 var insertImageAndDecorateObject = (arr, idx, maxlength, callback)=> {
-    if(idx >= maxlength) {
+    if (idx >= maxlength) {
         console.log('all images streamed', arr[0].images);
 
         return callback(arr);
@@ -129,8 +142,7 @@ function streaming(filenameFromTitle, ext, path, arr, idx, maxlength, objectProp
     });
 }
 
-function replaceIllegalChars(string)
-{
+function replaceIllegalChars(string) {
     let value = string.toLowerCase();
     value = value.replace(/ä/g, 'ae');
     value = value.replace(/ö/g, 'oe');
@@ -144,80 +156,80 @@ function replaceIllegalChars(string)
     return value + '.';
 }
 /*
-    let location = {
-        "_id": "012bb2568b1842959293402b06c98e15",
-        "_rev": "10-635f9c7df3c2dec7fa6ded84e842d5da",
-        "type": "location",
-        "userid": "89e09a2739e6ca2d544e779fbdc62693",
-        "preLocation": false,
-        "create_date": "2015-07-21T15:21:48.252Z",
-        "images": {
-            "picture": "/api/v1/locations/012bb2568b1842959293402b06c98e15/supertrip.jpeg",
-            "googlemap": "https://maps.googleapis.com/maps/api/staticmap?zoom=15&markers=48.269733,7.721381999999949"
-        },
-        "modified_date": "2015-11-30T15:44:49.025Z",
-        "tags": [
-            "Europa",
-            "Freizeitpark",
-            "spaß",
-            "achterbahn",
-            "rust",
-            "kindheitserinnerungen"
-        ],
-        "title": "Europa Park",
-        "description": "Schönen Tag im Europa Park verbringen!",
-        "city": {
-            "title": "Rust, Deutschland",
-            "place_id": "ChIJrR6otoE5kUcRIIbfpbdrHwQ",
-            "id": "ChIJrR6otoE5kUcRIIbfpbdrHwQ"
-        },
-        "geotag": {
-            "long": 7.721381999999949,
-            "lat": 48.269733
-        },
-        "delete": false,
-        "public": true,
-        "schoenhiers": 0,
-        "_attachments": {
-            "mobileThumb": {
-                "content_type": "image/jpeg",
-                "revpos": 7,
-                "digest": "md5-i1vDIBawl8bseJgBEpg8tA==",
-                "length": 9333,
-                "stub": true
-            },
-            "small": {
-                "content_type": "image/jpeg",
-                "revpos": 6,
-                "digest": "md5-7JF+FvDrrdAwW1NupKf03w==",
-                "length": 42130,
-                "stub": true
-            },
-            "max": {
-                "content_type": "image/jpeg",
-                "revpos": 5,
-                "digest": "md5-gPoWz1EXU47fBwtbE5hGJw==",
-                "length": 424787,
-                "stub": true
-            },
-            "mobile": {
-                "content_type": "image/jpeg",
-                "revpos": 4,
-                "digest": "md5-DY1sSnjNjlni32b7ETBt7Q==",
-                "length": 89601,
-                "stub": true
-            },
-            "mid": {
-                "content_type": "image/jpeg",
-                "revpos": 2,
-                "digest": "md5-PU19Ud0MqYuhRU6SLIGl0A==",
-                "length": 121291,
-                "stub": true
-            }
-        }
-    }
+ let location = {
+ "_id": "012bb2568b1842959293402b06c98e15",
+ "_rev": "10-635f9c7df3c2dec7fa6ded84e842d5da",
+ "type": "location",
+ "userid": "89e09a2739e6ca2d544e779fbdc62693",
+ "preLocation": false,
+ "create_date": "2015-07-21T15:21:48.252Z",
+ "images": {
+ "picture": "/api/v1/locations/012bb2568b1842959293402b06c98e15/supertrip.jpeg",
+ "googlemap": "https://maps.googleapis.com/maps/api/staticmap?zoom=15&markers=48.269733,7.721381999999949"
+ },
+ "modified_date": "2015-11-30T15:44:49.025Z",
+ "tags": [
+ "Europa",
+ "Freizeitpark",
+ "spaß",
+ "achterbahn",
+ "rust",
+ "kindheitserinnerungen"
+ ],
+ "title": "Europa Park",
+ "description": "Schönen Tag im Europa Park verbringen!",
+ "city": {
+ "title": "Rust, Deutschland",
+ "place_id": "ChIJrR6otoE5kUcRIIbfpbdrHwQ",
+ "id": "ChIJrR6otoE5kUcRIIbfpbdrHwQ"
+ },
+ "geotag": {
+ "long": 7.721381999999949,
+ "lat": 48.269733
+ },
+ "delete": false,
+ "public": true,
+ "schoenhiers": 0,
+ "_attachments": {
+ "mobileThumb": {
+ "content_type": "image/jpeg",
+ "revpos": 7,
+ "digest": "md5-i1vDIBawl8bseJgBEpg8tA==",
+ "length": 9333,
+ "stub": true
+ },
+ "small": {
+ "content_type": "image/jpeg",
+ "revpos": 6,
+ "digest": "md5-7JF+FvDrrdAwW1NupKf03w==",
+ "length": 42130,
+ "stub": true
+ },
+ "max": {
+ "content_type": "image/jpeg",
+ "revpos": 5,
+ "digest": "md5-gPoWz1EXU47fBwtbE5hGJw==",
+ "length": 424787,
+ "stub": true
+ },
+ "mobile": {
+ "content_type": "image/jpeg",
+ "revpos": 4,
+ "digest": "md5-DY1sSnjNjlni32b7ETBt7Q==",
+ "length": 89601,
+ "stub": true
+ },
+ "mid": {
+ "content_type": "image/jpeg",
+ "revpos": 2,
+ "digest": "md5-PU19Ud0MqYuhRU6SLIGl0A==",
+ "length": 121291,
+ "stub": true
+ }
+ }
+ }
 
 
-    // first long, then lat!!
-    var point = { type: "Point", coordinates: [ 40, 5 ] }
-*/
+ // first long, then lat!!
+ var point = { type: "Point", coordinates: [ 40, 5 ] }
+ */
